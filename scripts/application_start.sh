ccefb5a8ef678ad0964fa73124a33e6fe32b0aa9
AravindanSundar <98455506+AravindanSundar@users.noreply.github.com>
2024-05-22 22:15:28 +0530

    AS: 22-05-24: adding sudo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-codedeploy-pipeline
-pm2 start app.js --name==nodejs-express-app
+pm2 start app.js --name=nodejs-express-app
 pm2 save
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+sudo pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
