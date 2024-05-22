9d299178af80d5201acc941324f1291c39f743c9
AravindanSundar <98455506+AravindanSundar@users.noreply.github.com>
2024-05-22 21:43:27 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+pm2 start app.js --name==nodejs-express-app
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
